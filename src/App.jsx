6214fe6df2afd5549f30bc66f6635ef15e94836e
adi-uchiha <adi.cs.night@gmail.com>
2023-05-09 13:01:07 +0530

    add doc docx support

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,26 @@ import {
 import { storage } from "./firebase";
 import { v4 } from "uuid";
 import PdfDisplay from "./components/PdfDisplay";
+import DocDisplay from "./components/DocDisplay";
 
 function App() {
+  // Upload State
   const [imageUpload, setImageUpload] = useState(null);
+  // Fetching states
   const [imageUrls, setImageUrls] = useState([]);
   const [pdfUrls, setPdfUrls] = useState([]);
+  const [docUrls, setDocUrls] = useState([]);
   const [loading ,setLoading] = useState(false)
-  const [fileList, setFileList] = useState([] )
-  const [fileType, setFileType] = useState("")
+  // Ref state for delete function
+  const [docFileRefList, setDocFileRefList] = useState([] )
+  const [pdfFileRefList, setPdfFileRefList] = useState([] )
 
+  // Validate file type in frontend
+  const [fileType, setFileType] = useState("")
+  // Folder Refs
   const imagesListRef = ref(storage, "images/");
   const pdfListRef = ref(storage, "pdf/");
+  const docListRef = ref(storage, "doc/");
 
   const uploadImage = () => {
     const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
@@ -49,16 +58,35 @@ function App() {
     });
   }
 
+  const uploadDoc = () => {
+    const imageRef = ref(storage, `doc/${imageUpload.name + v4()}`);
+    uploadBytes(imageRef, imageUpload).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url) => {
+        console.log(url)
+        setImageUrls((prev) => [...prev, url]);
+        setLoading(false);
+        setImageUpload("") 
+        window.alert("Success")
+        window.location.reload()
+      });
+    });
+  }
+
+
   const upload = () => {
     if (imageUpload == null){ alert("Please select file"); return}
     setLoading(true)
+    console.log(fileType)
     if(fileType.startsWith('image')) {
       uploadImage(); 
       return
     } else if(fileType === 'application/pdf') {
       uploadPdf(); 
       return
-    } else alert("File type not supported, Please upload pdf, jpeg, jpg, png only")
+    } else if(fileType === 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' || fileType === 'application/msword') {
+      uploadDoc()
+      return
+    } else alert("File type not supported, Please upload pdf, doc, docx, image only")
   }
 
   useEffect(() => {
@@ -73,9 +101,20 @@ function App() {
     // fetch pdfs
     listAll(pdfListRef).then((response) => {
       response.items.forEach((item) => {
-        setFileList((prev)=>[...prev, item])
+        //This for delete function
+        // setFileList((prev)=>[...prev, item]) 
         getDownloadURL(item).then((url) => {
           setPdfUrls((prev)=> [...prev,url]);
+        });
+      }); 
+    });
+
+    // Fetch Docs
+    listAll(docListRef).then((response) => {
+      response.items.forEach((item) => {
+        setDocFileRefList((prev)=>[...prev, item])
+        getDownloadURL(item).then((url) => {
+          setDocUrls((prev)=> [...prev,url]);
         });
       }); 
     });
@@ -92,12 +131,16 @@ function App() {
       />
       <button onClick={upload}>{loading ? "Loading": "Upload"}</button>
       <br />
+      {docUrls.map((url, index) => {
+        return <DocDisplay downloadUrl={url} fileRef={docFileRefList[0]} key={index}/>;
+      })}
+      <br />
       {imageUrls.map((url, index) => {
         return <img src={url} key={index}/>;
       })}
       <br />
       {pdfUrls.map((url, index) => {
-        return <PdfDisplay downloadUrl={url} fileRef={fileList[0]} key={index}/>;
+        return <PdfDisplay downloadUrl={url} fileRef={docFileRefList[0]} key={index}/>;
       })}
 
     </div>
---
+++ src/components/DocDisplay.jsx
@@ -0,0 +1,46 @@
+import { deleteObject } from 'firebase/storage'
+import './PdfDisplay.css'
+import { useState } from 'react'
+
+const DocDisplay = (props) => {
+  console.log(props)
+
+  const [loading, setLoading] = useState(false)
+
+  const firebaseUrl = props?.downloadUrl
+  const regex = /%2F(.*?)\.doc/;
+  const match = regex.exec(firebaseUrl);
+  match[1];
+  const fileName = decodeURIComponent(match[1]) + '.doc'
+
+
+  function handleDelete() {
+    setLoading(true)
+    console.log("Delete")
+    deleteObject(props.fileRef).then(()=> {
+      alert("Gyi file lul, Refresh plix")
+      window.location.reload()
+    })
+  }
+  
+  return <>
+  <div className='pdf-display'> 
+    <div className='pdf-icon'>
+      <img className='pdf-svg' src='/doc.svg'></img>
+    </div>
+
+    <div className="pdf-name">{fileName}</div>
+
+    <div onClick={handleDelete} className="delete">
+      {loading ? "loading..." : <img onClick={handleDelete} className='delete-svg' src='/delete.svg'></img>}
+    </div>
+    <div className="download-icon">
+    <a href={firebaseUrl} download="" target="_blank" rel="noreferrer">
+      <img className='download-svg' src='/cloud-download-icon.svg'></img>
+    </a>
+    </div>
+  </div>
+  </>
+}
+
+export default DocDisplay
